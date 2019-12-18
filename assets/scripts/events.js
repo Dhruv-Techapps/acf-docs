e1545935264178aa2a0d52a4cc4b4e68c0f7864a
Dhruv Techapps <dhruv.techapps@gmail.com>
2019-12-18 11:11:43 +0530

    fixed blank page issue

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -2,7 +2,7 @@ window.onload = function () {
  $("#click-events-test").bind("mouseover mousedown mouseup click dblclick", function (event) {
   console.log(event.type, event.currentTarget);
  });
- $("#form-events-test").bind("backspace", function (event) {
-  console.log(event.type, event.currentTarget);
+ $("#form-events-test").bind("blur focus keypress", function (event) {
+  console.log(event.type, event.currentTarget, event.keyCode, event.which);
  });
 }
