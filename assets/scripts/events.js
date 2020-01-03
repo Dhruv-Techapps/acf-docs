8b1c7330ca3ef2f704c3d3573a1d92212caaf489
Dharmesh Patel <Dharmesh.patel@pb.com>
2020-01-03 14:22:43 +0530

    moving domain name

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,8 +1,8 @@
 window.onload = function () {
- $("#click-events-test").bind("mouseover mousedown mouseup click dblclick", function (event) {
+ $("#click-events-test").bind("mouseover mousedown mouseup click dblclick blur focus submit", function (event) {
   console.log(event.type, event.currentTarget);
  });
- $("#form-events-test").bind("blur focus keypress", function (event) {
-  console.log(event.type, event.currentTarget, event.keyCode, event.which);
+ $("#form-events-test").bind("blur focus submit keypress keydown keyup", function (event) {
+  console.log(event.type, event.key, event.keyCode, event);
  });
 }
