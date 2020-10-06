d23811dfeacb7463d0a598d19a3de168076e2cc3
Dharmesh <dharmesh.hemaram@gmail.com>
2020-10-06 15:54:30 +0530

    published dev version

---
+++ options_page_dev/precache-manifest.028fee652c62a3f1314f7df79588944d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb45aa83e3f116593274642e46dac65c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dab3b6bd1bd1b89772fa",
+    "url": "./static/css/main.d17d5d91.chunk.css"
+  },
+  {
+    "revision": "549c94e9d1b3c1178238",
+    "url": "./static/js/2.b6a03015.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "./static/js/2.b6a03015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dab3b6bd1bd1b89772fa",
+    "url": "./static/js/main.38a84deb.chunk.js"
+  },
+  {
+    "revision": "28f457ad2593d55e70b6",
+    "url": "./static/js/runtime-main.913d3d45.js"
+  },
+  {
+    "revision": "593a9f7b9d2ecea8cdf7e4ad645106a2",
+    "url": "./static/media/caret-down-fill.593a9f7b.svg"
+  },
+  {
+    "revision": "f876641e8536898e366bbe94a0093568",
+    "url": "./static/media/caret-down.f876641e.svg"
+  },
+  {
+    "revision": "2e8439a2a5f8cb55ee9a0d6cb4f1f1c3",
+    "url": "./static/media/caret-up-fill.2e8439a2.svg"
+  },
+  {
+    "revision": "c51f7df21a238a475a5e881400fdb5c9",
+    "url": "./static/media/caret-up.c51f7df2.svg"
+  },
+  {
+    "revision": "e20a343a6b4b53372909862563590e48",
+    "url": "./static/media/code-slash.e20a343a.svg"
+  },
+  {
+    "revision": "c3ccfceedd998a059fafba121553eb66",
+    "url": "./static/media/filter-right.c3ccfcee.svg"
+  },
+  {
+    "revision": "d1ddcdfb0c4b356d19ab1d7974a7e89c",
+    "url": "./static/media/gear-fill.d1ddcdfb.svg"
+  },
+  {
+    "revision": "fdca81a6fe185281c41c8331c2c45a77",
+    "url": "./static/media/info-circle.fdca81a6.svg"
+  },
+  {
+    "revision": "6d2428afc2b67bd6536e8ada194b5813",
+    "url": "./static/media/three-dots-vertical.6d2428af.svg"
+  },
+  {
+    "revision": "4f4d0149c8d5d1e6e8c47404a0ab7436",
+    "url": "./static/media/volume-mute.4f4d0149.svg"
+  },
+  {
+    "revision": "8ec53ce92866f169e2a29a0ff1cf96f2",
+    "url": "./static/media/volume-up.8ec53ce9.svg"
+  },
+  {
+    "revision": "4923419249168a4c407172353b4b3eef",
+    "url": "./static/media/x-circle.49234192.svg"
+  }
+]);
